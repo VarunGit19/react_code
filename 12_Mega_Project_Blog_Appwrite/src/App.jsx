7627605da93e942851e1f9ee544fa3117224d997
Varun <varundixit1906@gmail.com>
2024-11-11 16:32:46 +0530

    Added store using redux toolkit and configured the state

--- 12_Mega_Project_Blog_Appwrite/src/App.jsx
+++ 12_Mega_Project_Blog_Appwrite/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import {Outlet} from 'react-router-dom'
 
-function App() {
-  console.log(import.meta.env_VITE_APPWRITE_URL);
-  
+function App() {  
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(() => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex-wrap flex content-between bg-gray-400'>
+
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+
+    </div>
+    
+  ) : null
 }
 
 export default App
